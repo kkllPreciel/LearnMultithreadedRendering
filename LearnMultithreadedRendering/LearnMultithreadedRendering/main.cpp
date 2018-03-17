8c13b1840b3a1f23ccdd77d9ff4869a290dc89d8
Unknown <zie35-gymiko@jcom.zaq.ne.jp>
2018-03-17 21:16:52 +0900

    メモリリーク検出用デバッグコードを追加

--- LearnMultithreadedRendering/LearnMultithreadedRendering/main.cpp
+++ LearnMultithreadedRendering/LearnMultithreadedRendering/main.cpp
@@ -1,8 +1,19 @@
 #include "learn_multithreaded_rendering.h"
 #include <QtWidgets/QApplication>
 
+// デバッグ(メモリリーク)用
+#if defined(_WIN32) && !defined(QT_NO_DEBUG)
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+#endif
+
 int main(int argc, char *argv[])
 {
+#if defined(_WIN32) && !defined(QT_NO_DEBUG)
+  _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
     QApplication a(argc, argv);
     LearnMultithreadedRendering w;
     w.show();
