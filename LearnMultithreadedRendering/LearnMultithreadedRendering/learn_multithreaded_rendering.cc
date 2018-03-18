b363a1bd86087a65a51a5e977557a24c4e80d347
Unknown <zie35-gymiko@jcom.zaq.ne.jp>
2018-03-18 23:57:27 +0900

    アプリケーションクラスがレンダラーを所持するように

--- LearnMultithreadedRendering/LearnMultithreadedRendering/learn_multithreaded_rendering.cc
+++ LearnMultithreadedRendering/LearnMultithreadedRendering/learn_multithreaded_rendering.cc
@@ -1,7 +1,12 @@
 #include "learn_multithreaded_rendering.h"
 
 LearnMultithreadedRendering::LearnMultithreadedRendering(QWidget *parent)
-    : QWidget(parent)
+    : QWidget(parent), renderer_(nullptr)
 {
     ui.setupUi(this);
+}
+
+LearnMultithreadedRendering::~LearnMultithreadedRendering()
+{
+  renderer_.reset();
 }
--- LearnMultithreadedRendering/LearnMultithreadedRendering/learn_multithreaded_rendering.h
+++ LearnMultithreadedRendering/LearnMultithreadedRendering/learn_multithreaded_rendering.h
@@ -1,15 +1,20 @@
 #pragma once
 
+#include <memory>
 #include <QtWidgets/QWidget>
 #include "ui_learn_multithreaded_rendering.h"
+#include "renderer/renderer.h"
 
 class LearnMultithreadedRendering : public QWidget
 {
     Q_OBJECT
 
 public:
-    LearnMultithreadedRendering(QWidget *parent = Q_NULLPTR);
+  LearnMultithreadedRendering(QWidget *parent = Q_NULLPTR);
+  ~LearnMultithreadedRendering();
+  
 
 private:
-    Ui::LearnMultithreadedRenderingClass ui;
+  Ui::LearnMultithreadedRenderingClass ui;
+  std::shared_ptr<App::IRenderer> renderer_; ///< ƒŒƒ“ƒ_ƒ‰[
 };
