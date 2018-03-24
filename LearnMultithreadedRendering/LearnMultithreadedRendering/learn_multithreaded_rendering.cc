52082bb915981aa003b1c56ecd2dc66e7ad0d1af
Unknown <zie35-gymiko@jcom.zaq.ne.jp>
2018-03-24 15:36:56 +0900

    メインループを作成

--- LearnMultithreadedRendering/LearnMultithreadedRendering/learn_multithreaded_rendering.cc
+++ LearnMultithreadedRendering/LearnMultithreadedRendering/learn_multithreaded_rendering.cc
@@ -1,12 +1,44 @@
 #include "learn_multithreaded_rendering.h"
 
+/**
+ *  @brief  コンストラクタ
+ *  @param  parent:親ウィジェット
+ */
 LearnMultithreadedRendering::LearnMultithreadedRendering(QWidget *parent)
     : QWidget(parent), renderer_(nullptr)
 {
     ui.setupUi(this);
+
+    // レンダラーの作成
+    renderer_ = App::IRenderer::Create(reinterpret_cast<HWND>(this->winId()), this->width(), this->height());
+
+    // メインループ呼び出し設定
+    timer = std::make_unique<QTimer>();
+    connect(timer.get(), SIGNAL(timeout()), this, SLOT(MainLoop()));
+    timer->start(1000 / 60);
 }
 
+/**
+ *  @brief  デストラクタ
+ */
 LearnMultithreadedRendering::~LearnMultithreadedRendering()
 {
   renderer_.reset();
+
+  if (timer->isActive())
+  {
+    timer->stop();
+  }
+}
+
+/**
+ *  @brief  メインループ
+ */
+void LearnMultithreadedRendering::MainLoop()
+{
+  // レンダリング開始(コマンドバッファを作成し、GPUに処理を投げる)
+
+  // ゲームオブジェクトの更新
+
+  // レンダリング終了待ち(GPUの処理終了待ち)
 }
--- LearnMultithreadedRendering/LearnMultithreadedRendering/learn_multithreaded_rendering.h
+++ LearnMultithreadedRendering/LearnMultithreadedRendering/learn_multithreaded_rendering.h
@@ -2,19 +2,34 @@
 
 #include <memory>
 #include <QtWidgets/QWidget>
+#include <qtimer.h>
 #include "ui_learn_multithreaded_rendering.h"
 #include "renderer/renderer.h"
 
 class LearnMultithreadedRendering : public QWidget
 {
-    Q_OBJECT
+  Q_OBJECT
 
 public:
+  /**
+   *  @brief  コンストラクタ
+   *  @param  parent:親ウィジェット
+   */
   LearnMultithreadedRendering(QWidget *parent = Q_NULLPTR);
+
+  /**
+   *  @brief  デストラクタ
+   */
   ~LearnMultithreadedRendering();
-  
+
+private Q_SLOTS:
+  /**
+   *  @brief  メインループ
+   */
+  void MainLoop(); 
 
 private:
   Ui::LearnMultithreadedRenderingClass ui;
-  std::shared_ptr<App::IRenderer> renderer_; ///< レンダラー
+  std::unique_ptr<QTimer> timer;              ///< タイマー
+  std::shared_ptr<App::IRenderer> renderer_;  ///< レンダラー
 };
