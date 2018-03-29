4903d845b0c1e56b8359e384c10a9cea359eafb4
Unknown <zie35-gymiko@jcom.zaq.ne.jp>
2018-03-29 23:35:16 +0900

    レンダリングを別スレッドで行うように

--- LearnMultithreadedRendering/LearnMultithreadedRendering/renderer/renderer.cc
+++ LearnMultithreadedRendering/LearnMultithreadedRendering/renderer/renderer.cc
@@ -7,6 +7,9 @@
  */
 
  // include
+#include <thread>
+#include <atomic>
+#include <mutex>
 #include <Sein/Direct3D12/direct3d12_device.h>
 #include "renderer/renderer.h"
 
@@ -23,7 +26,7 @@ namespace App
       /**
        *  @brief  コンストラクタ
        */
-      Renderer() : device_(nullptr), execute_command_list_(nullptr), store_command_list_(nullptr)
+      Renderer() : device_(nullptr), execute_command_list_(nullptr), store_command_list_(nullptr), thread_(nullptr), terminate_(false), processing_(false)
       {
 
       }
@@ -51,6 +54,11 @@ namespace App
         // コマンドリストの作成
         execute_command_list_ = device_->CreateCommandList(D3D12_COMMAND_LIST_TYPE::D3D12_COMMAND_LIST_TYPE_DIRECT);
         store_command_list_ = device_->CreateCommandList(D3D12_COMMAND_LIST_TYPE::D3D12_COMMAND_LIST_TYPE_DIRECT);
+
+        // スレッドの作成
+        terminate_ = false;
+        processing_ = false;
+        thread_ = std::make_unique<std::thread>(&Renderer::ThreadMain, this);
       }
 
       /**
@@ -58,26 +66,9 @@ namespace App
        */
       void Execute() override
       {
-        // 実行用コマンドリストと作成用コマンドリストを交換する
-        execute_command_list_.swap(store_command_list_);
-
-        // オブジェクトの描画に必要なもの
-        // ワールド座標
-        // カメラ
-        // プロジェクション
-        // メッシュ
-        // ライト
-        // ボーン行列
-
-        // 前フレームで作成したコマンドのリストを実行する(GPU)
-        device_->ExecuteCommandLists(execute_command_list_.get());
-
-        // TODO:前フレームのゲーム情報からコマンドのリストを作成する(キューに格納されている予定)
-
-        // TODO:BeginScene、EndScene内のリソース指定を変更できるように
-        auto buffer_index = device_->GetNextBackBufferIndex();
-        device_->BeginScene(store_command_list_.get(), buffer_index);
-        device_->EndScene(store_command_list_.get(), buffer_index);
+        // レンダリングスレッドを起動する
+        processing_ = true;
+        condition_.notify_all();
       }
       
       /**
@@ -85,6 +76,11 @@ namespace App
        */
       void Present() override
       {
+        // レンダリングスレッドの処理の終了待ちを行う
+        // ここに来るまでに処理が終了している可能性がある
+        std::unique_lock<std::mutex> lock(mutex_);
+        condition_.wait(lock, [&] {return false == processing_; });
+
         // TODO:描画終了待ちを行う
 
         // 画面の更新を行う
@@ -96,12 +92,72 @@ namespace App
        */
       void Destroy() override
       {
+        // レンダリングスレッドの終了待ちを行う
+        if (thread_ && thread_->joinable())
+        {
+          terminate_ = true;
+          condition_.notify_all();
+          thread_->join();
+          thread_.reset();
+        }
+
         store_command_list_.reset();
         execute_command_list_.reset();
         device_.reset();
       }
 
     private:
+      /**
+       *  @brief  スレッドのメイン関数
+       */
+      void ThreadMain()
+      {
+        // 終了フラグが立っていなければ実行
+        while (terminate_ == false)
+        {
+          // スレッドの待機
+          std::unique_lock<std::mutex> lock(mutex_);
+          condition_.wait(lock, [&] { return processing_ || terminate_; });
+
+          if (terminate_)
+          {
+            continue;
+          }
+
+          // 実行用コマンドリストと作成用コマンドリストを交換する
+          execute_command_list_.swap(store_command_list_);
+
+          // 前フレームで作成したコマンドのリストを実行する(GPU)
+          device_->ExecuteCommandLists(execute_command_list_.get());
+
+          // TODO:前フレームのゲーム情報からコマンドのリストを作成する(キューに格納されている予定)
+
+          // オブジェクトの描画に必要なもの
+          // ワールド座標
+          // カメラ
+          // プロジェクション
+          // メッシュ
+          // ライト
+          // ボーン行列
+
+          // TODO:BeginScene、EndScene内のリソース指定を変更できるように
+          auto buffer_index = device_->GetNextBackBufferIndex();
+          device_->BeginScene(store_command_list_.get(), buffer_index);
+          device_->EndScene(store_command_list_.get(), buffer_index);
+
+
+          // スレッドの処理終了通知
+          processing_ = false;
+          condition_.notify_all();
+        }
+      }
+
+    private:
+      std::unique_ptr<std::thread> thread_;                                   ///< スレッド
+      std::atomic<bool> terminate_;                                           ///< 終了フラグ
+      bool processing_;                                                       ///< 処理中フラグ
+      std::mutex mutex_;                                                      ///< スレッド間排他処理用
+      std::condition_variable condition_;                                     ///< スレッド間実行待機用
       std::unique_ptr<Sein::Direct3D12::Device> device_;                      ///< デバイス
       std::shared_ptr<Sein::Direct3D12::ICommandList> execute_command_list_;  ///< 実行用コマンドリスト
       std::shared_ptr<Sein::Direct3D12::ICommandList> store_command_list_;    ///< 作成用コマンドリスト
