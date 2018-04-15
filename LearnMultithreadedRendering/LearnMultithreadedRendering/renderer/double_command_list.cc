b40cbbd471ed604ff0c7e04eadab851dd0649a45
Unknown <zie35-gymiko@jcom.zaq.ne.jp>
2018-04-15 23:58:04 +0900

    2個のコマンドリストを管理するクラスを作成

---
+++ LearnMultithreadedRendering/LearnMultithreadedRendering/renderer/double_command_list.cc
@@ -0,0 +1,101 @@
+/**
+ *  @file     double_command_list.cc
+ *  @brief    ダブルコマンドリストに関する処理を行うプログラムソース
+ *  @author   kkllPreciel
+ *  @date     2018/04/15
+ *  @version  1.0
+ */
+
+ // include
+#include "renderer/double_command_list.h"
+
+namespace App
+{
+  namespace
+  {
+    class DoubleCommandList final : public IDoubleCommandList
+    {
+    public:
+      /**
+       *  @brief  コンストラクタ
+       */
+      DoubleCommandList() : front_(nullptr), back_(nullptr)
+      {
+
+      }
+    
+      /**
+       *  @brief  デストラクタ
+       */
+      ~DoubleCommandList() override
+      {
+        Destroy();
+      }
+
+      /**
+       *  @brief  コマンドリストを作成する
+       *  @param  device:デバイス
+       *  @param  command_list_type:コマンドリストのタイプ
+       */
+      void Create(Sein::Direct3D12::Device* const device, const D3D12_COMMAND_LIST_TYPE& command_list_type)
+      {
+        front_ = device->CreateCommandList(command_list_type);
+        back_ = device->CreateCommandList(command_list_type);
+      }
+  
+      /**
+       *  @brief  コマンドリストの交換を行う
+       */
+      void Swap() override
+      {
+        front_.swap(back_);
+      }
+  
+      /**
+       *  @brief  現在フロントのコマンドリストを取得する
+       *  @return コマンドリストへの参照
+       */
+      const Sein::Direct3D12::ICommandList& GetFrontCommandList() override
+      {
+        return *front_;
+      }
+      
+      /**
+       *  @brief  現在バックのコマンドリストを取得する
+       *  @return コマンドリストへの参照
+       */
+      Sein::Direct3D12::ICommandList& GetBackCommandList() override
+      {
+        return *back_;
+      }
+    
+      /**
+       *  @brief  終了処理を行う
+       */
+      void Destroy() override
+      {
+        front_.reset();
+        back_.reset();
+      }
+
+    private:
+      std::shared_ptr<Sein::Direct3D12::ICommandList> front_; ///< フロントのコマンドリスト
+      std::shared_ptr<Sein::Direct3D12::ICommandList> back_;  ///< バックのコマンドリスト
+    };
+  };
+
+  /**
+   *  @brief  ダブルコマンドリストを作成する
+   *  @param  device:デバイス
+   *  @param  command_list_type:コマンドリストのタイプ
+   *  @return ダブルコマンドリストインターフェイスへのシェアードポインタ
+   */
+  std::shared_ptr<IDoubleCommandList> IDoubleCommandList::Create(Sein::Direct3D12::Device* const device, const D3D12_COMMAND_LIST_TYPE& command_list_type)
+  {
+    auto double_command_list = std::make_shared<DoubleCommandList>();
+
+    double_command_list->Create(device, command_list_type);
+
+    return double_command_list;
+  }
+};
---
+++ LearnMultithreadedRendering/LearnMultithreadedRendering/renderer/double_command_list.h
@@ -0,0 +1,77 @@
+/**
+ *  @file     double_command_list.h
+ *  @brief    ダブルコマンドリストに関するヘッダファイル
+ *  @author   kkllPreciel
+ *  @date     2018/04/15
+ *  @version  1.0
+ */
+
+#pragma once
+
+// include
+#include <memory>
+#include <Sein/Direct3D12/command_list.h>
+#include <Sein/Direct3D12/direct3d12_device.h>
+
+namespace App
+{
+  /**
+   *  @brief  ダブルバッファリングを行うコマンドリスト用インターフェイス
+   */
+  class IDoubleCommandList
+  {
+  public:
+    /**
+     *  @brief  コンストラクタ
+     */
+    IDoubleCommandList() = default;
+  
+    /**
+     *  @brief  デストラクタ
+     */
+    virtual ~IDoubleCommandList() = default;
+
+    /**
+     *  @brief  コマンドリストの交換を行う
+     */
+    virtual void Swap() = 0;
+
+    /**
+     *  @brief  現在フロントのコマンドリストを取得する
+     *  @return コマンドリストへの参照
+     */
+    virtual const Sein::Direct3D12::ICommandList& GetFrontCommandList() = 0;
+    
+    /**
+     *  @brief  現在バックのコマンドリストを取得する
+     *  @return コマンドリストへの参照
+     */
+    virtual Sein::Direct3D12::ICommandList& GetBackCommandList() = 0;
+  
+    /**
+     *  @brief  終了処理を行う
+     */
+    virtual void Destroy() = 0;
+  
+    /**
+     *  @brief  コピーコンストラクタ
+     *  @param  other:コピー元のインスタンス
+     */
+    IDoubleCommandList(const IDoubleCommandList& other) = delete;
+  
+    /**
+     *  @brief  代入演算子オペレータ
+     *  @param  other:代入元のインスタンス
+     *  @return 代入後のインスタンス
+     */
+    IDoubleCommandList& operator = (const IDoubleCommandList& other) = delete;
+
+    /**
+     *  @brief  ダブルコマンドリストを作成する
+     *  @param  device:デバイス
+     *  @param  command_list_type:コマンドリストのタイプ
+     *  @return ダブルコマンドリストインターフェイスへのシェアードポインタ
+     */
+    static std::shared_ptr<IDoubleCommandList> Create(Sein::Direct3D12::Device* const device, const D3D12_COMMAND_LIST_TYPE& command_list_type);
+  };
+};
